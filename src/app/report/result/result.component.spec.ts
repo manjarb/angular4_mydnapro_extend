f5e5abeae5100de67fd2085e52dc92cbeb326a97
Varis <manjarb4@gmail.com>
2017-11-05 11:17:35 +0700

    Update User test and result test

--- src/app/report/result/result.component.spec.ts
+++ src/app/report/result/result.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ResultComponent } from './result.component';
+import { GeneticService } from '../../service/genetic.service'
+
 
 describe('ResultComponent', () => {
   let component: ResultComponent;
@@ -19,7 +21,18 @@ describe('ResultComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should have correct Result list item Count', () => {
+    const fixture = TestBed.createComponent(ResultComponent);
+    const app = fixture.debugElement.componentInstance;
+    let geneticService = fixture.debugElement.injector.get(GeneticService);
+    fixture.detectChanges();
+    let geneticResult = geneticService.getResult();
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const list_item = fixture.nativeElement.query(By.css('.result-list-item'));
+
+      expect(geneticResult.length).toBe(list_item.length);
+    });
     expect(component).toBeTruthy();
   });
 });
--- src/app/shared/user-profile/user-profile.component.spec.ts
+++ src/app/shared/user-profile/user-profile.component.spec.ts
@@ -1,25 +1,46 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {} from 'jasmine';
 
 import { UserProfileComponent } from './user-profile.component';
+// import { AuthService } from '../../service/auth.service'
 
 describe('UserProfileComponent', () => {
-  let component: UserProfileComponent;
-  let fixture: ComponentFixture<UserProfileComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ UserProfileComponent ]
-    })
-    .compileComponents();
-  }));
+  // let component: UserProfileComponent;
+  // let fixture: ComponentFixture<UserProfileComponent>;
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserProfileComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [UserProfileComponent]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create app', () => {
+    const fixture = TestBed.createComponent(UserProfileComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should Render First name and Last name Correctly', () => {
+    const fixture = TestBed.createComponent(UserProfileComponent);
+    const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const header = fixture.debugElement.query(By.css('h2'));
+      const el = header.nativeElement;
+
+      expect(el.value).toBe(`${app.user.firstname} ${app.user.lastname}`);
+    });
+  });
+
+  it('should Render Email and Date of Birth Correctly', () => {
+    const fixture = TestBed.createComponent(UserProfileComponent);
+    const app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const header = fixture.debugElement.query(By.css('h4'));
+      const el = header.nativeElement;
+
+      expect(el.value).toBe(` ${app.user.email} ${app.user.dob}` );
+    });
   });
 });
